54a23a6fff2a85a48fdfc88583a38e55200442c0
solsen <spam2steve@gmail.com>
2019-01-28 15:43:24 -0500

    Updates

---
+++ precache-manifest.2b8a4ae11d74612176883266c0dce168.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d53d57e405a7cf0b07ab",
+    "url": "/static/js/runtime~main.d53d57e4.js"
+  },
+  {
+    "revision": "d1948723ecefd5d662a4",
+    "url": "/static/js/main.d1948723.chunk.js"
+  },
+  {
+    "revision": "0fce1eaf502f66ce5cf6",
+    "url": "/static/js/2.0fce1eaf.chunk.js"
+  },
+  {
+    "revision": "d1948723ecefd5d662a4",
+    "url": "/static/css/main.15ec12ac.chunk.css"
+  },
+  {
+    "revision": "c6ed29f6c0fa2c24b2960a8792a7addc",
+    "url": "/index.html"
+  }
+];
